e8774a64f6e1cc58f4a8f477e36b959b5dba4c23
Assadmessi <ayehtetheinmessi@gmail.com>
2023-02-03 16:59:28 +0630

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Circles from './Componets/Circles';
+
+function App() {
+  return (
+    <div className="app">
+      <div>
+        <Circles />
+      </div>
+    </div>
+  );
+}
+
+export default App;
